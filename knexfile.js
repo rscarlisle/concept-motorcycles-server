d817b55073afe08f8d5d357cc721ec578816fb6a
Randall Carlisle <cisolutionsinc@gmail.com>
2018-05-17 15:11:31 -0700

    used heroku config command to generate knexfile db_url

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,6 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: 'postgres://whyzxclvfrbtzi:0a0ffb7953eddaa1381036dd0e768d86946e7c0780c4410940041a16b8c7bfa3@ec2-23-21-129-50.compute-1.amazonaws.com:5432/d7nuh6araec7dl'
   }  
 };
